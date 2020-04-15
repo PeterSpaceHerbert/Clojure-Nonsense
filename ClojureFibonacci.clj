677eca9ebdf30f1c8ac3cc9abf3efc3b5810c66b
PeterSpaceHerbert <63022828+PeterSpaceHerbert@users.noreply.github.com>
2020-04-15 09:32:55 -0400

    Update and rename ClojureFibonacci.cl to ClojureFibonacci.clj
    Finally made the godforsaken thing print out the list of the Fibonacci made. Though, it is backwards...

--- ClojureFibonacci.clj
+++ ClojureFibonacci.clj
@@ -2,22 +2,23 @@
 
 (defn main []
   (println "Please input two numbers to start a psuedo-fibonacci sequence: " )
+  (def fib-seq '())
   (let [num1String (read-line) num2String (read-line) num1 (read-string num1String) num2 (read-string num2String)]
-    (fibonacci num1 num2))
-    (println "Ending program...")
-    (print "Final fibonacci in reverse order: ")
-    (print (rest fib-seq))
-)
+    (fibonacci num1 num2 fib-seq)))
 
-(def fib-seq '())
-
-(defn fibonacci [num1 num2]
+(defn fibonacci [num1 num2 num-seq]
   (def sum (+ num1 num2))
+  (def fib-list (conj num-seq sum))
   (println "The new value generated is" sum)
-  (conj fib-seq :sum)
   (println "Go again? y/n:")
   (let [goAgain (read-line)]
     (if (= goAgain "y")
-      (recur num2 sum))))
+      (recur num2 sum fib-list)
+      (output fib-list))))
+
+(defn output [num-seq]
+  (println "Ending program...")
+  (print "Psuedo-Fibonacci sequence generated:")
+  (print num-seq))
 
 (main)
